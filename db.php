454c70c82d5132a670aca0e61e04641dbd3d4565
Zhanabek Maksotuly <zhanabekmaksot@gmail.com>
2019-12-20 02:01:17 +0600

    php files

--- db.php
+++ db.php
@@ -6,17 +6,28 @@ $password = "";
 
 $db = new PDO("mysql:host=$dbhost;dbname=$dbname",$username,$password);
 
-//database-дегы данныйларды кайтару
+//return information on database
 function get_all_platformas(){
     global $db;
     $singles = $db->query("SELECT * FROM main");
     return $singles;
 }
-//id бойынша тексеру
+//check with ID
 function get_category_by_id($id){
     global $db;
     $categories = $db->query("SELECT * FROM categories WHERE id=$id");
     foreach($categories as $category){
         return $category["category_name"];
+    }
+}
+
+
+
+//return information on database by id
+function get_platforma_by_id($id){
+    global $db;
+    $singles = $db->query("SELECT * FROM main WHERE id=$id");
+    foreach($singles as $single){
+        return $single;
     }
 }
--- product_list.php
+++ product_list.php
@@ -1,32 +1,31 @@
 <?php require "blocks/header.php";?>
+<?php $single = get_platforma_by_id($_GET["id"]); ?>
+<?php $category = get_category_by_id($single["id_category"]);?>
 <div class="backgr">
-<div class="category">
-            <a href="Football">hjgfd</a>
-            <a href="">kljhgf</a>
-            <a href="">jkhgfd</a>
-            <a href="">kljhgfd</a>
-</div>       
+        <div class="category">
+                <a href="Football">hjgfd</a>
+                <a href="">kljhgf</a>
+                <a href="">jkhgfd</a>
+                <a href="">kljhgfd</a>
+        </div> 
         <div class="cont">
-            <?php
-                $singles = get_all_platformas();
-                foreach($singles as $single) : ?>
-                <?php $category = get_category_by_id($single["id_category"]);?>
                 <div class="content">
                         <div class="all">
-                                <div class="platforma">
+                                <div class="platforma1">
                                         <a href=""><i class="fas fa-shopping-basket"></i></a>
-                                        <a href="Product_list.php?id=<?php echo $single["id"]; ?>"><img src="<?php echo $single["img"]; ?>" alt="photo"></a>
+                                        <div class="img_p"><img src="<?php echo $single["img"]; ?>" alt="photo"></div>
+                                        <div class="list_p">
                                         <p>Category:<?php echo $category; ?></p>
                                         <p>Name:<?php echo $single["name"]; ?></p>
                                         <p>Costs:<?php echo $single["cost"]; ?>$</p>
                                         <p>Sizes:<?php echo $single["size"]; ?></p>
                                         <p>Colors:<?php echo $single["color"]; ?></p>
                                         <button>Buy</button>
+                                        </div>
                                 </div>
                         </div>
                 </div>
-                <?php endforeach; ?>    
-        </div>
             
+        </div>
+</div>     
 <?php require "blocks/footer.php";?>
-</div>
